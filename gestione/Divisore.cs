b7560e8040e7c3ee33d0c6fada8643c837493c03
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-30 10:50:29 +0100

    divisori di un numero

---
+++ gestione/Divisore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Gestione
+{
+    public class Divisore
+    {
+        public static int LeggiNumero()
+        {
+            Console.Write("inserisci un numero intero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        public static void StampaDivisori(int n)
+        {
+            int i = 1;
+            int max = n / 2;
+            while (i<=max)
+            {
+                if ( n% i==0)
+                    Console.WriteLine(i);
+                i++;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
